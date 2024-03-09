3deb99da053fc01dc49b0904769abd34b9932bb6
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-09 22:56:35 +0530

    Controllers

--- src/main/java/com/products/controller/CategoryController.java
+++ src/main/java/com/products/controller/CategoryController.java
@@ -1,2 +1,78 @@
-package com.products.controller;public class CategoryController {
+package com.products.controller;
+
+import com.products.model.Categories;
+import com.products.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("api/categories/")
+public class CategoryController {
+    @Autowired
+    private CategoryService categoryService;
+
+    /*
+    *
+    * Add Category
+    *
+    * */
+    @PostMapping
+    public ResponseEntity<String> addCategory(@RequestBody Categories newCategory){
+        if(categoryService.addCategory(newCategory)){
+            return ResponseEntity.ok("Category added successfully!");
+        }
+        return ResponseEntity.ok("Category already exists!");
+    }
+
+    /*
+    *
+    * Update Category
+    *
+    * */
+    @PutMapping("{id}")
+    public ResponseEntity<String> updateCategory(@PathVariable Long id ,@RequestBody Categories updateCategory){
+        if (categoryService.updateCategory(id, updateCategory)){
+            return ResponseEntity.ok("Successfully updated category id " + id);
+        }
+        return ResponseEntity.ok("Invalid category id " + id);
+    }
+
+    /*
+    *
+    * Get All Categories
+    *
+    * */
+    @GetMapping
+    public ResponseEntity<List<Categories>> getAllCategories(){
+        return ResponseEntity.ok(categoryService.getAllCategories());
+    }
+
+    /*
+    *
+    * Get Category by id
+    *
+    * */
+    @GetMapping("{id}")
+    public ResponseEntity<Object> getCategoryByID(@PathVariable("id") Long categoryId){
+        Categories dbCategory = categoryService.getCategoryById(categoryId);
+
+        return (dbCategory == null) ? ResponseEntity.ok("Invalid category id "+categoryId) : ResponseEntity.ok(dbCategory);
+    }
+
+    /*
+    *
+    * Delete Category
+    *
+    * */
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteCategoryById(@PathVariable("id") Long categoryId){
+        return categoryService.deleteCategoryById(categoryId) ?
+                ResponseEntity.ok("Successfully deleted category having id "+categoryId) : ResponseEntity.ok("Invalid category id "+categoryId);
+    }
+
+
 }
--- src/main/java/com/products/controller/ProductController.java
+++ src/main/java/com/products/controller/ProductController.java
@@ -1,2 +1,101 @@
-package com.products.controller;public class ProductController {
+package com.products.controller;
+
+import com.products.model.Categories;
+import com.products.model.CategoryResponse;
+import com.products.model.Product;
+import com.products.model.ProductDetailsResponse;
+import com.products.service.CategoryService;
+import com.products.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /*
+    *
+    * Add Product
+    *
+    * */
+    @PostMapping
+    public ResponseEntity<String> addProduct(@RequestBody Product product) {
+        return productService.addProduct(product) ? ResponseEntity.ok("Product added successfully") : ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to add product");
+    }
+
+    /*
+    *
+    * Get product By Id
+    *
+    * */
+    @GetMapping("{id}")
+    public ResponseEntity<ProductDetailsResponse> getProductById(@PathVariable("id") Long productId){
+        Product product = productService.getProductById(productId);
+        if (product == null) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Categories category = product.getCategory();
+        if (category == null) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null);
+        }
+
+        ProductDetailsResponse response = new ProductDetailsResponse(product.getId(), product.getName(), product.getCost(), new CategoryResponse(category.getCategoryId(), category.getCategoryName()));
+
+        return ResponseEntity.ok(response);
+    }
+
+
+    /*
+    *
+    * Get All Products
+    *
+    * */
+    @GetMapping
+    public ResponseEntity<List<ProductDetailsResponse>> getAllProducts() {
+        List<Product> products = productService.getAllProducts();
+        List<ProductDetailsResponse> responseList = new ArrayList<>();
+
+        for (Product product : products) {
+            Categories category = product.getCategory();
+            if (category != null) {
+                CategoryResponse categoryResponse = new CategoryResponse(category.getCategoryId(), category.getCategoryName());
+                ProductDetailsResponse productDetailsResponse = new ProductDetailsResponse(product.getId(), product.getName(), product.getCost(), categoryResponse);
+                responseList.add(productDetailsResponse);
+            }
+        }
+
+        return ResponseEntity.ok(responseList);
+    }
+
+
+    /*
+    *
+    * Delete Product by Id
+    *
+    * */
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProductById(@PathVariable("id") Long productId) {
+        return productService.deleteProductById(productId) ? ResponseEntity.ok("Product deleted successfully") : ResponseEntity.notFound().build();
+    }
+
+    /*
+    *
+    * Update Product by Id
+    *
+    * */
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateProduct(@PathVariable("id") Long productId, @RequestBody Product updatedProduct) {
+        return productService.updateProduct(productId, updatedProduct) ? ResponseEntity.ok("Product updated successfully") : ResponseEntity.notFound().build();
+    }
 }
