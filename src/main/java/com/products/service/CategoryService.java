993542063507a598fd99caef771f79d34641a74d
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-10 00:53:06 +0530

    Service

--- src/main/java/com/products/service/CategoryService.java
+++ src/main/java/com/products/service/CategoryService.java
@@ -5,7 +5,7 @@ import com.products.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
