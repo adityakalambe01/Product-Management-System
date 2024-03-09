3a8eacb1549f640f455c367666fc5c094629a98d
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-09 22:56:11 +0530

    Services

--- src/main/java/com/products/service/CategoryService.java
+++ src/main/java/com/products/service/CategoryService.java
@@ -1,4 +1,124 @@
 package com.products.service;
 
+import com.products.model.Categories;
+import com.products.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
 public class CategoryService {
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    /*
+    *
+    * Get all Categories
+    *
+    * */
+    public List<Categories> getAllCategories(){
+        return categoryRepository.findAll();
+    }
+
+    /*
+    *
+    * Get Category by Id
+    *
+    * */
+    public Categories getCategoryById(long categoryID){
+        Categories categories;
+        try{
+            categories = categoryRepository.findById(categoryID).get();
+        }catch(Exception e){
+            return null;
+        }
+        return categories;
+    }
+
+    /*
+    *
+    * Delete Category by Id
+    *
+    * */
+    public Boolean deleteCategoryById(long categoryID){
+
+        try{
+            Categories categories = categoryRepository.findById(categoryID).get();
+            categoryRepository.delete(categories);
+        }catch(Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    /*
+    *
+    * Add Category
+    *
+    * */
+    public Boolean addCategory(Categories category){
+        try {
+            Categories dbCategory = categoryRepository.findByCategoryName(category.getCategoryName().trim());
+            if (dbCategory != null)
+                throw new IllegalArgumentException("Category already exists!");
+            categoryRepository.save(category);
+        }catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+    /*
+    *
+    * Update Category
+    *
+    * */
+    public Boolean updateCategory(Long categoryIdFromBrowser ,Categories updateCategory){
+        try {
+            Categories dbCategory = categoryRepository.findById(categoryIdFromBrowser).orElse(null);
+            if (dbCategory == null)
+                throw new IllegalArgumentException("Category not found!");
+            dbCategory.setCategoryName(updateCategory.getCategoryName());
+            dbCategory.setCategoryDescription(updateCategory.getCategoryDescription());
+            categoryRepository.save(dbCategory);
+        }catch (Exception e){
+            return false;
+        }
+        return true;
+    }
+
+
+    @PostConstruct
+    public void addDefaultCategories() {
+        if (categoryRepository.count() == 0) {
+            Map<String,String> productCategories= new HashMap<>();
+
+            productCategories.put("Electronics and Gadgets", "Including smartphones, laptops, tablets, cameras, and accessories like chargers and headphones.");
+            productCategories.put("Apparel and Accessories", "Clothing items, shoes, bags, jewelry, and other fashion accessories.");
+            productCategories.put("Home and Kitchen Appliances", "Appliances for cooking, cleaning, and home improvement, such as refrigerators, washing machines, vacuum cleaners, and kitchen gadgets.");
+            productCategories.put("Health and Beauty Products", "Including skincare products, cosmetics, hair care items, vitamins, and supplements.");
+            productCategories.put("Sports and Outdoor Equipment", "Gear for various sports activities, camping equipment, hiking gear, bicycles, and fitness accessories.");
+            productCategories.put("Toys and Games", "Including toys for children of all ages, board games, puzzles, and outdoor play equipment.");
+            productCategories.put("Books and Stationery", "Books across various genres, notebooks, pens, office supplies, and art materials.");
+            productCategories.put("Furniture and Home Decor", "Including indoor and outdoor furniture, home decor items like rugs, curtains, and wall art.");
+            productCategories.put("Food and Beverages", "Grocery items, beverages, snacks, spices, and specialty foods.");
+            productCategories.put("Automotive Parts and Accessories", "Parts for vehicles, car accessories, maintenance products, and automotive tools.");
+
+
+
+
+            for (Map.Entry<String, String> entry : productCategories.entrySet()) {
+                Categories newCategory = new Categories();
+                newCategory.setCategoryName(entry.getKey());
+                newCategory.setCategoryDescription(entry.getValue());
+                System.out.println(newCategory);
+                categoryRepository.save(newCategory);
+            }
+            System.out.println("Default categories added successfully");
+        }
+    }
 }
--- src/main/java/com/products/service/ProductService.java
+++ src/main/java/com/products/service/ProductService.java
@@ -1,2 +1,83 @@
-package com.products.service;public class ProductService {
+package com.products.service;
+
+import com.products.model.*;
+import com.products.repositories.CategoryRepository;
+import com.products.repositories.ProductRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    /*
+    *
+    * Add Product
+    *
+    * */
+    public boolean addProduct(Product product) {
+
+        productRepository.save(product);
+        return true;
+    }
+
+    /*
+    *
+    * Get Product By Id
+    *
+    * */
+    public Product getProductById(Long productId) {
+        Optional<Product> dbProduct = productRepository.findById(productId);
+        return dbProduct.orElse(null);
+    }
+
+    /*
+    *
+    * Delete Product by Id
+    *
+    * */
+    public boolean deleteProductById(Long productId) {
+        if (productRepository.existsById(productId)) {
+            productRepository.deleteById(productId);
+            return true;
+        }
+        return false;
+    }
+
+    /*
+    *
+    * Get all Products
+    *
+    * */
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    /*
+    *
+    * Update Product
+    *
+    * */
+    public boolean updateProduct(Long productId, Product updatedProduct) {
+        Optional<Product> optionalProduct = productRepository.findById(productId);
+        if (optionalProduct.isPresent()) {
+            Product product = optionalProduct.get();
+            BeanUtils.copyProperties(updatedProduct, product, "id");
+            productRepository.save(product);
+            return true;
+        }
+        return false;
+    }
+
+
+
+
 }
