cf478c0ff1f7a2701c7ea383a4b7647495e54c33
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-15 02:03:35 +0530

    Repository

--- src/main/java/com/products/repositories/ProductRepository.java
+++ src/main/java/com/products/repositories/ProductRepository.java
@@ -1,7 +1,14 @@
 package com.products.repositories;
 
+import com.products.model.Categories;
 import com.products.model.Product;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    @Modifying
+    @Transactional
+    void deleteByCategory(Categories categories);
 }
