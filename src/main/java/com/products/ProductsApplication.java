a46bab79321b3292d02d7b846a51d1de0cc35e3f
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-14 15:31:32 +0530

    Main

--- src/main/java/com/products/ProductsApplication.java
+++ src/main/java/com/products/ProductsApplication.java
@@ -1,23 +1,15 @@
 package com.products;
 
-import com.products.controller.CategoryController;
-import com.products.model.Categories;
-import com.products.repositories.CategoryRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 @SpringBootApplication
 public class ProductsApplication {
-
     public static void main(String[] args) {
-
         SpringApplication.run(ProductsApplication.class, args);
+
     }
 
 }
