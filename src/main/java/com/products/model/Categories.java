95a1dd272791cc0537f4b9c661a56697c2efa204
Aditya Kalambe <adityakalambe20@gmail.com>
2024-03-15 01:06:35 +0530

    Model

--- src/main/java/com/products/model/Categories.java
+++ src/main/java/com/products/model/Categories.java
@@ -2,10 +2,14 @@ package com.products.model;
 
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.util.List;
 
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "categories")
 public class Categories {
     @Id
@@ -22,90 +26,4 @@ public class Categories {
     @OneToMany(mappedBy = "category")
     private List<Product> products;
 
-    public Categories(Long categoryId, String categoryName, String categoryDescription, List<Product> products) {
-        this.categoryId = categoryId;
-        this.categoryName = categoryName;
-        this.categoryDescription = categoryDescription;
-        this.products = products;
-    }
-
-    public Categories() {
-    }
-
-    public Long getCategoryId() {
-        return this.categoryId;
-    }
-
-    public String getCategoryName() {
-        return this.categoryName;
-    }
-
-    public String getCategoryDescription() {
-        return this.categoryDescription;
-    }
-
-    public List<Product> getProducts() {
-        return this.products;
-    }
-
-    public void setCategoryId(Long categoryId) {
-        this.categoryId = categoryId;
-    }
-
-    public void setCategoryName(String categoryName) {
-        this.categoryName = categoryName;
-    }
-
-    public void setCategoryDescription(String categoryDescription) {
-        this.categoryDescription = categoryDescription;
-    }
-
-    public void setProducts(List<Product> products) {
-        this.products = products;
-    }
-
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Categories)) return false;
-        final Categories other = (Categories) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$categoryId = this.getCategoryId();
-        final Object other$categoryId = other.getCategoryId();
-        if (this$categoryId == null ? other$categoryId != null : !this$categoryId.equals(other$categoryId))
-            return false;
-        final Object this$categoryName = this.getCategoryName();
-        final Object other$categoryName = other.getCategoryName();
-        if (this$categoryName == null ? other$categoryName != null : !this$categoryName.equals(other$categoryName))
-            return false;
-        final Object this$categoryDescription = this.getCategoryDescription();
-        final Object other$categoryDescription = other.getCategoryDescription();
-        if (this$categoryDescription == null ? other$categoryDescription != null : !this$categoryDescription.equals(other$categoryDescription))
-            return false;
-        final Object this$products = this.getProducts();
-        final Object other$products = other.getProducts();
-        if (this$products == null ? other$products != null : !this$products.equals(other$products)) return false;
-        return true;
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof Categories;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $categoryId = this.getCategoryId();
-        result = result * PRIME + ($categoryId == null ? 43 : $categoryId.hashCode());
-        final Object $categoryName = this.getCategoryName();
-        result = result * PRIME + ($categoryName == null ? 43 : $categoryName.hashCode());
-        final Object $categoryDescription = this.getCategoryDescription();
-        result = result * PRIME + ($categoryDescription == null ? 43 : $categoryDescription.hashCode());
-        final Object $products = this.getProducts();
-        result = result * PRIME + ($products == null ? 43 : $products.hashCode());
-        return result;
-    }
-
-    public String toString() {
-        return "Categories(categoryId=" + this.getCategoryId() + ", categoryName=" + this.getCategoryName() + ", categoryDescription=" + this.getCategoryDescription() + ", products=" + this.getProducts() + ")";
-    }
 }
--- src/main/java/com/products/model/Product.java
+++ src/main/java/com/products/model/Product.java
@@ -1,8 +1,13 @@
 package com.products.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
+@Data
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "products")
 public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -18,88 +23,4 @@ public class Product {
     @ManyToOne
 //    @JsonIgnore
     private Categories category;
-
-    public Product(Long id, String name, Double cost, Categories category) {
-        this.id = id;
-        this.name = name;
-        this.cost = cost;
-        this.category = category;
-    }
-
-    public Product() {
-    }
-
-    public Long getId() {
-        return this.id;
-    }
-
-    public String getName() {
-        return this.name;
-    }
-
-    public Double getCost() {
-        return this.cost;
-    }
-
-    public Categories getCategory() {
-        return this.category;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setCost(Double cost) {
-        this.cost = cost;
-    }
-
-    public void setCategory(Categories category) {
-        this.category = category;
-    }
-
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof Product)) return false;
-        final Product other = (Product) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$id = this.getId();
-        final Object other$id = other.getId();
-        if (this$id == null ? other$id != null : !this$id.equals(other$id)) return false;
-        final Object this$name = this.getName();
-        final Object other$name = other.getName();
-        if (this$name == null ? other$name != null : !this$name.equals(other$name)) return false;
-        final Object this$cost = this.getCost();
-        final Object other$cost = other.getCost();
-        if (this$cost == null ? other$cost != null : !this$cost.equals(other$cost)) return false;
-        final Object this$category = this.getCategory();
-        final Object other$category = other.getCategory();
-        if (this$category == null ? other$category != null : !this$category.equals(other$category)) return false;
-        return true;
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof Product;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $id = this.getId();
-        result = result * PRIME + ($id == null ? 43 : $id.hashCode());
-        final Object $name = this.getName();
-        result = result * PRIME + ($name == null ? 43 : $name.hashCode());
-        final Object $cost = this.getCost();
-        result = result * PRIME + ($cost == null ? 43 : $cost.hashCode());
-        final Object $category = this.getCategory();
-        result = result * PRIME + ($category == null ? 43 : $category.hashCode());
-        return result;
-    }
-
-    public String toString() {
-        return "Product(id=" + this.getId() + ", name=" + this.getName() + ", cost=" + this.getCost() + ", category=" + this.getCategory() + ")";
-    }
 }
